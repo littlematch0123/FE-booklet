1b3670f29688173a5f8736f2e036683af2d82af6
xiaohuochai <121631835@qq.com>
2017-11-05 02:12:16 +0800

    更改“HTML标签嵌套规则”中的一个链接

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,40 @@
 var gulp = require('gulp');
-var imagemin = require('gulp-imagemin');
-gulp.task('img', function () {
-  gulp.src('img/*')
-    .pipe(imagemin([
-      imagemin.gifsicle({ interlaced: true }),
-      imagemin.jpegtran({ progressive: true }),
-      imagemin.optipng({ optimizationLevel: 5 }),
-      imagemin.svgo({
-        plugins: [
-          { removeViewBox: true },
-          { cleanupIDs: false }
-        ]
-      })
-    ]))
-    .pipe(gulp.dest('dist/img'));
-
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var cleanCSS = require('gulp-clean-css');
+var merge = require('gulp-merge-link');
+var connect = require('gulp-connect');
+gulp.task('merge', function () {
+  gulp.src(['_book/HTML/**/*.html'])
+    .pipe(merge({
+      '../../base.css': ['../../**/*.css'],
+      '../../base.js': ['../../**/*.js'],
+    }))
+    .pipe(gulp.dest('dist/html/'));
+  gulp.src(['_book/*.html'])
+    .pipe(merge({
+      'base.css': ['**/*.css'],
+      'base.js': ['**/*.js'],
+    }))
+    .pipe(gulp.dest('dist'));    
 });
-
-gulp.task('default',['img']);
+gulp.task('move',function(){
+  gulp.src('_book/search_plus_index.json')
+  .pipe(gulp.dest('dist'));
+  gulp.src('_book/gitbook/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts'))
+})
+gulp.task('concat',function(){
+  gulp.src('_book/**/*.js')
+    .pipe(concat('base.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
+  gulp.src('_book/**/*.css')
+    .pipe(concat('base.css'))
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(gulp.dest('dist'));
+})
+gulp.task('connect', function () {
+  connect.server();
+});
+gulp.task('default',['merge','concat','connect','move']);
