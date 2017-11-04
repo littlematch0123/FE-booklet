ef7256a1131d18d696a374514b3f97524c609fd2
xiaohuochai <121631835@qq.com>
2017-11-05 00:40:12 +0800

    ？

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,17 @@ var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
 gulp.task('img', function () {
   gulp.src('img/*')
-    .pipe(imagemin())
+    .pipe(imagemin([
+      imagemin.gifsicle({ interlaced: true }),
+      imagemin.jpegtran({ progressive: true }),
+      imagemin.optipng({ optimizationLevel: 5 }),
+      imagemin.svgo({
+        plugins: [
+          { removeViewBox: true },
+          { cleanupIDs: false }
+        ]
+      })
+    ]))
     .pipe(gulp.dest('dist/img'));
 
 });
